3dcbb177a46defebc610be1878300707dfa2a870
Allison <allisonkbates@gmail.com>
2020-12-14 10:59:29 -0700

    adding modal styles

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let openPresent = document.getElementById('open-present');
+let viewRecipe = document.getElementById('view-recipe');
 let drinkCard = document.getElementById('drink-card');
+let modal = document.getElementById('modal');
 
 function showDrink() {
 	drinkCard.classList.toggle('drink__card--hide');
 	openPresent.classList.toggle('present__button--hide');
-	console.log('Yay!')
+	console.log('open gift!')
 }
-	
 
+function showRecipe() {
+	modal.classList.toggle('modal__container--hide')
+	console.log('show modal!')
+}
+
+viewRecipe.addEventListener('click', showRecipe);
 openPresent.addEventListener('click', showDrink);
