349d35167ae2a66788ad98ac2ff6f61acb2c06e9
Allison <allisonkbates@gmail.com>
2020-12-14 16:51:21 -0700

    added modal functionality and made it work with all the cards

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let openPresent = document.getElementById('open-present');
-let viewRecipe = document.getElementById('view-recipe');
-let drinkCard = document.getElementById('drink-card');
-let modal = document.getElementById('modal');
+let giftButtons = document.querySelectorAll('.open-gift-button');
+let recipeButtons = document.querySelectorAll('.view-recipe-button');
+let closeButtons = document.querySelectorAll('.close-button');
+let closeDrinks = document.querySelectorAll('.close-drink');
 
-function showDrink() {
-	drinkCard.classList.toggle('drink__card--hide');
-	openPresent.classList.toggle('present__button--hide');
-	console.log('open gift!')
+function showDrink(e) {
+	const giftNumber = e.target.dataset.key;
+	const buttonNumber = giftNumber.replace('gift', 'button');
+	console.log(giftNumber, buttonNumber);
+	const drinkCard = document.querySelector(`div[data-key="${giftNumber}"]`);
+	console.log(drinkCard);
+	const openButton = document.querySelector(`button[data-key="${giftNumber}"]`);
+	console.log(openButton);
+  drinkCard.classList.toggle('drink__card--hide');
+	openButton.classList.toggle('present__button--hide');
 }
 
-function showRecipe() {
-	modal.classList.toggle('modal__container--hide')
-	console.log('show modal!')
+function viewRecipe(e) {
+	const recipeNumber = e.target.dataset.key;
+	const modal = document.querySelector(`div[data-key="${recipeNumber}"]`);
+	modal.classList.toggle('modal__container--hide');
 }
 
-viewRecipe.addEventListener('click', showRecipe);
-openPresent.addEventListener('click', showDrink);
+giftButtons.forEach(giftButton => giftButton.addEventListener('click', showDrink));
+recipeButtons.forEach(recipeButton => recipeButton.addEventListener('click', viewRecipe));
+closeButtons.forEach(closeButton => closeButton.addEventListener('click', viewRecipe));
+closeDrinks.forEach(closeDrink => closeDrink.addEventListener('click', showDrink));
